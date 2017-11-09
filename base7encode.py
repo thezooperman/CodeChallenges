17b2be99c4021c86782dc86087109e033b0e52bf
Joy <joy.calcutta@gmail.com>
2017-11-09 10:02:16 +0530

    Updated codes list

--- base7encode.py
+++ base7encode.py
@@ -1,6 +1,6 @@
 def encodetobase7(value):
     output = []
-    codes = ['0', 'a', 't', 'l', 's', 'i', 'n']
+    codes = ['o', 'a', 't', 'l', 's', 'i', 'n']
     while value > 0:
         output.append(codes[value % 7])
         value = value // 7
