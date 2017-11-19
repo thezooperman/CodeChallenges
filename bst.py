fde0f5f931a59edec609bba05ba5b3018279ecd7
Aritra <joy.calcutta@gmail.com>
2017-11-19 10:50:33 +0530

    Added Binary Search Tree class file

---
+++ bst.py
@@ -0,0 +1,13 @@
+'''
+This module lists all the basic Binary Tree/Binary Search Tree interview questions
+'''
+class Node:
+    '''Class to define a Binary Tree Node'''
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+class BST:
+    '''Binary Search Tree'''
+    pass
