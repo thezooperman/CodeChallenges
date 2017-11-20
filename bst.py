9078e91e24fcc5d67cb11c7cd31c780f2076ccc3
Aritra <joy.calcutta@gmail.com>
2017-11-20 23:42:04 +0530

    Added postifx evaluator code

--- bst.py
+++ bst.py
@@ -10,4 +10,31 @@ class Node:
 
 class BST:
     '''Binary Search Tree'''
-    pass
+    def __init__(self):
+        self.root = None
+
+    def setRoot(self, data):
+        '''Add the root of BST
+           Args:
+                data - value, preferably int value, to store in
+                BST root node
+        '''
+        self.root = Node(data)
+
+    def insert(self, data):
+        '''Insert data into BST
+           Args:
+                data - value, preferably int value to store in
+                BST node
+                If data is less than root, traverse left until right
+                node is found to insert, else follow same logic on right
+                tree
+        '''
+        if self.root is not None:
+            if data < self.root.data:
+                if self.root.left is None:
+                    self.root.left = Node(data)
+                else:
+                    pass
+        else:
+            self.setRoot(data)
---
+++ postfix_evaluator.py
@@ -0,0 +1,20 @@
+'''Write a simple function to evaluate a
+postix expression and return the result'''
+def evaluate(input_text):
+    '''Given a postfix input string, return the value'''
+    ops = []
+    for i in input_text:
+        if str(i).isdigit():
+            ops.append(i)
+        else:
+            val1 = ops.pop()
+            val2 = ops.pop()
+            ops.append(str(eval(val2 + i + val1)))
+    return ops.pop()
+
+def main():
+    '''Entry point to fn'''
+    val = '5 3 + 8 2 - *'
+    print(evaluate(val.strip().split(' '))) #returns 48
+
+main()
