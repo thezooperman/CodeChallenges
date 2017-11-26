67791a2f9d9dfccc4b96c44647d1633bcace196f
Aritra <joy.calcutta@gmail.com>
2017-11-26 22:47:23 +0530

    added some formating - pylint

--- min_time.py
+++ min_time.py
@@ -18,16 +18,16 @@ def mintime(A, B, C, D, E, F):
     timeformat = "%H:%M:%S"
     valid_time = []
     for p in itertools.permutations([A, B, C, D, E, F]):
-        t = '{}{}:{}{}:{}{}'.format(*p)
+        str_time = '{}{}:{}{}:{}{}'.format(*p)
         try:
-            datetime.datetime.strptime(t, timeformat)
+            datetime.datetime.strptime(str_time, timeformat)
         except ValueError:
             pass
         else:
-            valid_time.append(t)
+            valid_time.append(str_time)
     return min(valid_time) if valid_time else 'NOT POSSIBLE'
 
-print(mintime(7,0,8,9,0,0)) #returns 07:08:09
-print(mintime(0,0,8,9,0,0)) #returns 00:08:09
-print(mintime(0,0,0,9,0,0)) #returns 00:00:09
-print(mintime(9,8,7,6,4,2)) #returns NOT POSSIBLE
+print(mintime(7, 0, 8, 9, 0, 0)) #returns 07:08:09
+print(mintime(0, 0, 8, 9, 0, 0)) #returns 00:08:09
+print(mintime(0, 0, 0, 9, 0, 0)) #returns 00:00:09
+print(mintime(9, 8, 7, 6, 4, 2)) #returns NOT POSSIBLE
