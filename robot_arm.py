cc86e1526cab14b33746478bf10995c8a174c675
Aritra <joy.calcutta@gmail.com>
2017-11-27 20:53:53 +0530

    Fixed code issues for robot arm

--- robot_arm.py
+++ robot_arm.py
@@ -1,33 +1,20 @@
-def convertToHexa(n):
-    '''Convert to Hexadecimal from decimal.
-        \rn: decimal value
-    '''
-    if n < 0:
-        return 0
-    elif n <= 1:
-        return n
-    else:
-        convertToHexa(n // 16)
-        x = n % 16
-        if x < 10:
-            return x
-        if x == 10:
-            return 'A'
-        if x == 11:
-            return 'B'
-        if x == 12:
-            return 'C'
-        if x == 13:
-            return 'D'
-        if x == 14:
-            return 'E'
-        if x == 15:
-            return 'F'
-
+'''A robot arm is programmed to do very basic instructions
+P for Pick block, M for Move block, and L for lower a block.
+The arm is forgetful and works with certain constraints.
+The arm always picks blocks from an infinite stash.
+There are 10 horizontal positions and 16 vertical positions to count
+blocks places by the arm (m by n matrix).
+The compute method accepts a series of sequences (P, M and L only - other
+instructions are ignored). There are couple of rules by which the arm operates.
+The objective is to return the block positions - that is, count of blocks generated
+after the sequence executes in hexadecimal form
+'''
 def  compute(instructions):
     '''Returns block state after the Robot carries out
         instructions.
         \rinstructions: string sequence of instructions'''
+    hex_code = [str(i) for i in range(10)]
+    [hex_code.append(chr(i)) for i in range(65, 71)]
     max_positions = 9
     blocks = ['0'] * max_positions
     current_pos = 0
@@ -45,10 +32,11 @@ def  compute(instructions):
             current_pos = current_pos % max_positions
         elif i == 'L':
             tmp = blocks[current_pos]
+            tmp = hex_code.index(str(blocks[current_pos]))
             if tmp:
-                tmp = int(str(tmp), 0)
+                # tmp = int(tmp, 0)
                 current_block += tmp
-            hex_block = convertToHexa(current_block)
+            hex_block = hex_code[current_block]
             if hex_block == 'F':
                 isheld = True
             else:
@@ -58,5 +46,5 @@ def  compute(instructions):
             current_block = 1 #reset current block counter
     print(''.join([str(s) for s in blocks]))
 
-compute('PMLPMMMLPMLPMML') #returns 0211000000
-compute('') #returns 000000000
+compute('PMLPMMMLPMLPMML') #returns 021100000
+compute('PLPLPLPLPLPLPLPLPLPL') #returns A00000000
