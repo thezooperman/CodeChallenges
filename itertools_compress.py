384def66c718bf7d0c8271e6e02748075c9faaf0
Aritra Ghosh <arghos@microsoft.com>
2018-01-31 00:00:08 +0530

    added pipenv

---
+++ itertools_compress.py
@@ -0,0 +1,12 @@
+"""
+You are given a string S. Suppose a character 'c' occurs
+consecutively X times in the string. Replace these
+consecutive occurrences of the character 'c' with (X,c) in the string. 
+"""
+
+
+import itertools
+
+string = input().strip()
+
+[print('(%s, %s) ' % (len(list(cgen)), c), end='') for c, cgen in itertools.groupby(string)]
