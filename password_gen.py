30be2af716d0e5397e3f052ad0f42e190f37be44
Aritra Ghosh <arghos@microsoft.com>
2018-02-13 17:43:25 +0530

    re-factored code

--- password_gen.py
+++ password_gen.py
@@ -1,6 +1,11 @@
-import string, random
+import string
+import random
 
-def genpassord(size = 8, chars = string.ascii_letters + string.punctuation):
+
+def genpassord(size=8, chars=string.ascii_letters + string.punctuation
+               + string.digits):
+    '''Password generation basic rule'''
     return ''.join([random.choice(chars) for _ in range(size)])
+
 
 print(genpassord(size=int(input('How many characters in password?').strip())))
