9b8ad4af39a01ba5fe3f126904a2d14cc5a45626
Zooperman <joy.calcutta@gmail.com>
2018-03-28 08:56:16 +0530

    added right rotation logic

--- array_rotation.py
+++ array_rotation.py
@@ -15,4 +15,15 @@ def array_rotation(array, rotate_by):
     return array
 
 
+def array_right_rotate(array, rotate_by):
+    length = len(array) - 1
+    for i in range(rotate_by):
+        temp = array[length]
+        for j in range(length, 0, -1):
+            array[j] = array[j - 1]
+        array[0] = temp
+    return array
+
+
 print(array_rotation([1, 2, 3, 4, 5, 6, 7], 2))
+print(array_right_rotate([1, 2, 3, 4, 5, 6, 7], 2))
