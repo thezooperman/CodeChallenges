860073a0d8925b0891f10b8de637418ab7758b85
Zooperman <joy.calcutta@gmail.com>
2018-04-05 08:57:32 +0530

    Print nodes at Kth distance from root

--- bst.py
+++ bst.py
@@ -3,9 +3,12 @@ This module lists all the basic Binary Tree/Binary
 Search Tree interview questions
 '''
 
+from collections import deque
+
 
 class Node:
     '''Class to define a Binary Tree Node'''
+
     def __init__(self, data):
         self.data = data
         self.left = None
@@ -14,6 +17,7 @@ class Node:
 
 class BST:
     '''Binary Search Tree'''
+
     def __init__(self):
         self.root = None
 
@@ -42,3 +46,54 @@ class BST:
                     pass
         else:
             self.setRoot(data)
+
+    def printKDistanceRecursive(self, root, k):
+        '''Print all nodes at Kth distance from root - Recursive'''
+        if root is None:
+            return
+        if k == 0:
+            print(root.data, end=' ')
+        else:
+            self.printKDistanceRecursive(root.left, k - 1)
+            self.printKDistanceRecursive(root.right, k - 1)
+
+    def printKDistantIterative(self, root, k):
+        '''Print all nodes at Kth distance from root'''
+        if root is None:
+            print(0)
+            return
+        q = deque()
+        q.append(root)
+        childQ = deque()
+        while q:
+            node = q.pop()
+            if node.left is not None:
+                childQ.appendleft(node.left)
+            if node.right is not None:
+                childQ.appendleft(node.right)
+            if len(q) == 0:
+                k -= 1
+                if k == 0:
+                    [print(val.data, end=' ') for val in reversed(childQ)]
+                    print()
+                q, childQ = childQ, deque()
+
+
+def main():
+    root = Node(1)
+    root.left = Node(2)
+    root.right = Node(3)
+    root.left.left = Node(4)
+    root.left.right = Node(5)
+    root.right.left = Node(8)
+    bst = BST()
+    bst.printKDistantIterative(root, 1)
+    bst.printKDistantIterative(root, 2)
+    bst.printKDistanceRecursive(root, 1)
+    print()
+    bst.printKDistanceRecursive(root, 2)
+    print()
+
+
+if __name__ == "__main__":
+    main()
