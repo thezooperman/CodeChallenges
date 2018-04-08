da9e22ef58165d98e63ddfc60cc1190431f0e123
Zooperman <joy.calcutta@gmail.com>
2018-04-08 17:58:56 +0530

    Added Vertical Tree Traversal and Optimized SumTree Logic

--- bst.py
+++ bst.py
@@ -3,7 +3,7 @@ This module lists all the basic Binary Tree/Binary
 Search Tree interview questions
 '''
 
-from collections import deque
+from collections import deque, defaultdict, OrderedDict
 import sys
 
 MAX_INT = sys.maxsize
@@ -139,6 +139,35 @@ class BST:
             return 0
         return self.getSum(node.left) + node.data + self.getSum(node.right)
 
+    def sumTreeOptimized(self, node):
+        '''Value at each non-leaf node is sum of its left
+        and right sub-trees. Use this trick to optimize  post-order Tree
+        traversal into O(n)'''
+        if node is None:
+            return 0
+        if node.left is None and node.right is None:
+            return node.data
+        left_sum = right_sum = 0
+        left_sum = self.sumTreeOptimized(node.left)
+        right_sum = self.sumTreeOptimized(node.right)
+        return 2 * node.data if node.data == left_sum + right_sum else MIN_INT
+
+    def verticalTraversal(self, root):
+        '''Print a Vertical Traversal of Tree View'''
+        hashMap = defaultdict(list)
+        self.verticalTraversalHelper(root, 0, hashMap)
+        sortedSet = OrderedDict(sorted(hashMap.items()))
+        {print(*v) for v in sortedSet.values()}
+        return
+
+    def verticalTraversalHelper(self, root, distance, hashMap):
+        '''Helper function for a Vertical Tree Traversal'''
+        if root is None:
+            return
+        hashMap.setdefault(distance, hashMap[distance].append(root.data))
+        self.verticalTraversalHelper(root.left, distance - 1, hashMap)
+        self.verticalTraversalHelper(root.right, distance + 1, hashMap)
+
 
 def main():
     root = Node(1)
@@ -151,14 +180,15 @@ def main():
     bst.printKDistantIterative(root, 1)
     bst.printKDistantIterative(root, 2)
     bst.printKDistanceRecursive(root, 1)
-    print()
+
     bst.printKDistanceRecursive(root, 2)
-    print()
+
     root = Node(10)
     root.left = Node(7)
     root.left.right = Node(11)
     root.right = Node(39)
     print(bst.isBST(root))
+    print('-'*20)
     root = Node(10)
     root.left = Node(8)
     root.left.left = Node(3)
@@ -172,6 +202,7 @@ def main():
         print()
     else:
         print(result)
+    print('-'*20)
     root = Node(44)
     root.left = Node(9)
     root.right = Node(13)
@@ -180,6 +211,17 @@ def main():
     root.right.left = Node(6)
     root.right.right = Node(7)
     print('Is Sum Tree == ', bst.sumTree(root))
+    print('Is Optimized Sum Tree == ', True if
+          bst.sumTreeOptimized(root) != MIN_INT else MIN_INT)
+    print('-'*20)
+    root = Node(1)
+    root.left = Node(2)
+    root.right = Node(3)
+    root.right.right = Node(6)
+    root.right.left = Node(5)
+    root.right.left.left = Node(7)
+    root.right.left.right = Node(8)
+    bst.verticalTraversal(root)
 
 
 if __name__ == "__main__":
