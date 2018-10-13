fa8ff3257fea78c1fee17730be3bddd3a0ca0c3c
Zooperman <joy.calcutta@gmail.com>
2018-10-13 12:18:16 +0530

    pylint suggested formatting

--- median_streaming_integer.py
+++ median_streaming_integer.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python3
+'''
+Given a streaming set of infinite integer variable,
+find the median of those variables, at any given point
+in time.
+
+O(n) = n log(n)
+'''
+
 import heapq
 
 
@@ -5,26 +14,35 @@ class StreamingInteger():
     def __init__(self):
         self.max_heap = []
         self.min_heap = []
+        self.count = 0
 
     def insert(self, num):
-        len_max = len(self.max_heap)
-        len_min = len(self.min_heap)
-        if (len_max + len_min) % 2 is 0:
+        if self.count % 2 is 0:
             heapq.heappush(self.max_heap, num * -1)
-            if len_min == 0:
+            self.count += 1
+            if len(self.min_heap) == 0:
                 return
             if (self.max_heap[0] * -1) > self.min_heap[0]:
                 # pop from max_heap and insert into min_heap
                 to_min = -1 * heapq.heappop(self.max_heap)
                 # pop from min_heap and insert into max_heap
-                to_max = heapq.heappop(self.min_heap)
+                to_max = -1 * heapq.heappop(self.min_heap)
                 heapq.heappush(self.max_heap, to_max)
                 heapq.heappush(self.min_heap, to_min)
         else:
             to_min = -1 * heapq.heappushpop(self.max_heap, -1 * num)
             heapq.heappush(self.min_heap, to_min)
+            self.count += 1
 
     def get_median(self):
-        if (len(self.max_heap) + len(self.min_heap)) % 2 == 0:
-            return ((-1 * self.max_heap[0] + self.min_heap[0]) / 2)
-        return -1 * self.max_heap[0]
+        if self.count % 2 == 0:
+            return (-1 * self.max_heap[0] + self.min_heap[0]) / 2.0
+        return float(-1 * self.max_heap[0])
+
+
+if __name__ == '__main__':
+    arr = [_ for _ in range(1, 11)]
+    stream = StreamingInteger()
+    for elem in arr:
+        stream.insert(elem)
+        print(stream.get_median())
