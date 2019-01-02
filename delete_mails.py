699064233201da85860d47275eee7d3f7a1dbe0f
Zooperman <joy.calcutta@gmail.com>
2019-01-02 21:06:27 +0530

    Changes: - Refactored code per pylint/flake8 - Added code to skip filters marked with # - Added more filters

--- delete_mails.py
+++ delete_mails.py
@@ -51,7 +51,8 @@ def batchDeleteMails(service):
         iterations = math.ceil(len(to_delete_ids) / batch_limit)
         skip, take = 0, batch_limit
         print(
-            f'Total Messages: {len(to_delete_ids)}, Target Iterations: {iterations}')
+            f'Total Messages: {len(to_delete_ids)},\
+            Target Iterations: {iterations}')
 
         batch = BatchHttpRequest(callback=deleteCallback)
 
@@ -65,7 +66,8 @@ def batchDeleteMails(service):
             ))
             skip = take
             take = len(to_delete_ids) if (take + batch_limit >=
-                                          len(to_delete_ids)) else take + batch_limit
+                                          len(to_delete_ids))\
+                else take + batch_limit
 
         batch.execute()
     except errors.HttpError as ex:
@@ -84,12 +86,14 @@ def getCallback(request_id, response, exception):
 
             while 'nextPageToken' in response:
                 page_token = response['nextPageToken']
-                response = service.users().messages().list(userId=USER_ID, q=request_id,
-                                                           pageToken=page_token).execute()
+                response = service.users().messages().list(
+                    userId=USER_ID,
+                    q=request_id,
+                    pageToken=page_token).execute()
                 messages.extend(response['messages'])
             to_delete_ids.extend(messages)
             print(f'{request_id} --> fetched messages: {len(messages)}')
-        except expression as identifier:
+        except errors.HttpError as ex:
             print(f'Exception:{ex}')
             return 1
 
@@ -106,7 +110,7 @@ service = getService()
 if __name__ == '__main__':
     mail_filter = []
     with open('mail_filter.txt', encoding='utf-8') as fp:
-        mail_filter = [line.strip() for line in fp]
+        mail_filter = [line.strip() for line in fp if not line.startswith('#')]
 
     getMailsByFilter(service, USER_ID, mail_filter)
     print('-' * 55)
