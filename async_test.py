8159383bd61df5369962a44419f5449a8b4b2666
Zooperman <joy.calcutta@gmail.com>
2020-04-24 22:14:07 +0530

    - Added the threading code

--- async_test.py
+++ async_test.py
@@ -3,13 +3,13 @@ import concurrent.futures
 import time
 
 
-async def my_task(task_number: int, seconds: int) -> str:
+async def my_asynctask(task_number: int, seconds: int) -> str:
     """
     A task to do for a number of seconds
     """
     # print('Task:{} is taking {} seconds to complete'.format(
     #     task_number, seconds))
-    await asyncio.sleep(seconds)
+    await asyncio.sleep(1.0 / task_number)
     return "Task:{} completed".format(task_number)
 
 
@@ -23,11 +23,16 @@ def my_thread_task(task_number: int, seconds: int) -> any:
     return "Task:{} completed".format(task_number)
 
 
-async def async_event(task_nos: int, event_loop: asyncio.AbstractEventLoop):
+async def async_event(task_nos: int, event_loop: asyncio.AbstractEventLoop) -> None:
+    event_loop = asyncio.events.get_running_loop()
     start = time.time()
     print("Creating {} async tasks...".format(task_nos))
-    tasks = {event_loop.create_task(my_task(i + 1, (i + 1) % 2)) for i in range(task_nos)}
-    await asyncio.gather(*tasks)
+    tasks = {event_loop.create_task(my_asynctask(i + 1, (i + 1) % 2)) for i in range(task_nos)}
+    await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
+    # await asyncio.gather(*tasks)
+    # for res in asyncio.as_completed(tasks):
+    # print_val(await res)
+    # print(await res)
     print("Time taken for async execution:", time.time() - start)
     # print([t.result() for t in tasks])
 
@@ -41,8 +46,8 @@ def thread_event(task_nos: int) -> concurrent.futures.Future:
         for task in concurrent.futures.as_completed(tasks):
             try:
                 yield task.result()
-            except Exception as ex:
-                print(ex)
+            except Exception as x:
+                print(x)
     print("Time taken for thread execution:", time.time() - start)
     # print([t for t in tasks])
 
@@ -53,13 +58,21 @@ if __name__ == '__main__':
     # async io
     loop = None
     try:
-        loop = asyncio.get_event_loop()
-        # asyncio.run(event(task_nos))
-        loop.run_until_complete(async_event(total_ops, loop))
+        # loop = asyncio.get_event_loop()
+        asyncio.run(async_event(total_ops, None))
+        # loop.run_until_complete(async_event(total_ops, loop))
     finally:
-        loop.close()
+        pass
+        # loop.close()
 
     # multi-threading
     for val in thread_event(total_ops):
-        pass
-        # print(val)
+        try:
+            # print(val)
+            pass
+        except TimeoutError as ter:
+            print(ter)
+        except concurrent.futures.CancelledError as cer:
+            print(cer)
+        except Exception as ex:
+            print(ex)
