fc428e45034bf5ccc1d8c4d7d90bada1235e1a95
Zooperman <joy.calcutta@gmail.com>
2020-04-26 18:03:17 +0530

    Re-worked on the async file

--- async_test.py
+++ async_test.py
@@ -1,14 +1,25 @@
 import asyncio
 import concurrent.futures
 import time
+import random
+import secrets
+import traceback
 
 
-async def my_asynctask(task_number: int, seconds: int) -> str:
+async def my_async_task(task_number: int, seconds: int) -> str:
     """
     A task to do for a number of seconds
     """
     # print('Task:{} is taking {} seconds to complete'.format(
     #     task_number, seconds))
+
+    # cancel the current task when random value hits 5
+    random_seed = random.randint(1, 10)
+    secret_seed = secrets.choice([i for i in range(1, 11)])
+    # print(random_seed, secret_seed, task_number)
+    if random_seed == 5 or secret_seed == 5:
+        # raise asyncio.CancelledError(task_number)
+        asyncio.current_task().cancel()
     await asyncio.sleep(1.0 / task_number)
     return "Task:{} completed".format(task_number)
 
@@ -19,22 +30,61 @@ def my_thread_task(task_number: int, seconds: int) -> any:
     """
     # print('Task:{} is taking {} seconds to complete'.format(
     #     task_number, seconds))
-    time.sleep(seconds)
-    return "Task:{} completed".format(task_number)
+    time.sleep(1.0 / task_number)
+    # print(concurrent.futures.thread.threading.currentThread().getName())
+    return "Thread:{} completed".format(task_number)
 
 
-async def async_event(task_nos: int, event_loop: asyncio.AbstractEventLoop) -> None:
-    event_loop = asyncio.events.get_running_loop()
+async def async_event(task_nos: int) -> None:
     start = time.time()
     print("Creating {} async tasks...".format(task_nos))
-    tasks = {event_loop.create_task(my_asynctask(i + 1, (i + 1) % 2)) for i in range(task_nos)}
-    await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
-    # await asyncio.gather(*tasks)
+
+    # ways to create a task
+    # can create from an event loop pre-python 3.7 era
+
+    # event_loop = asyncio.events.get_running_loop()
+    # tasks = {event_loop.create_task(my_async_task(i + 1, (i + 1) % 2)) for i in range(task_nos)}
+
+    # or just wrap the async tasks in a sequence and schedule it
+    # tasks = {my_async_task(i + 1, (i + 1) % 2) for i in range(task_nos)}
+
+    # wrap with create_task to pass as future object, used in wait or gather
+    tasks = {asyncio.create_task(my_async_task(i + 1, (i + 1) % 2)): (i + 1) for i in range(task_nos)}
+
+    # enumerate over the task as soon as they complete
+    # order guarantee is not maintained
     # for res in asyncio.as_completed(tasks):
-    # print_val(await res)
-    # print(await res)
+    #     try:
+    #         result = await res
+    #         print(result)
+    #     except (asyncio.CancelledError, asyncio.TimeoutError, Exception) as ce:
+    #         print("Traceback: {}".format(res))
+
+    # using wait to wait for tasks and handle exception
+    done, pending = await asyncio.wait(tasks, return_when=asyncio.ALL_COMPLETED)
+    for task in done:
+        try:
+            if not task.exception():
+                print(task.result())
+        except (asyncio.CancelledError, asyncio.TimeoutError, Exception) as er:
+            print("Error: Task-{} failed with {}".format(tasks[task], type(er).__name__))
+
+    # clear the pending task set
+    for pending_task in pending:
+        pending_task.cancel()
+
+    # example for asyncio.gather
+    # results = await asyncio.gather(*tasks, return_exceptions=True)  # results contains the completed result
+    # returned from the method
+
+    # for task in tasks:
+    #     try:
+    #         if not task.exception():
+    #             print(task.result())
+    #     except (asyncio.futures.CancelledError, asyncio.CancelledError, Exception) as ex:
+    #         print("Error: Task-{} failed with {}".format(tasks[task], type(ex).__name__))
+
     print("Time taken for async execution:", time.time() - start)
-    # print([t.result() for t in tasks])
 
 
 def thread_event(task_nos: int) -> concurrent.futures.Future:
@@ -53,13 +103,13 @@ def thread_event(task_nos: int) -> concurrent.futures.Future:
 
 
 if __name__ == '__main__':
-    total_ops = 1000
+    total_ops = 100000
 
     # async io
     loop = None
     try:
         # loop = asyncio.get_event_loop()
-        asyncio.run(async_event(total_ops, None))
+        asyncio.run(async_event(total_ops))
         # loop.run_until_complete(async_event(total_ops, loop))
     finally:
         pass
@@ -68,7 +118,7 @@ if __name__ == '__main__':
     # multi-threading
     for val in thread_event(total_ops):
         try:
-            # print(val)
+            print(val)
             pass
         except TimeoutError as ter:
             print(ter)
