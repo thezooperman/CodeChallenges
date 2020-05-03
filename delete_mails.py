0a892dbf35f96cbf6341a371fee6f112c45d32df
Zooperman <joy.calcutta@gmail.com>
2020-05-03 11:41:42 +0530

    Changes: - re-factored delete_mails.py to update the google api dependencies - used set to store e-mail milters instead of dict, fixes the duplicate entry error - updated the mail_filter.txt

--- delete_mails.py
+++ delete_mails.py
@@ -6,11 +6,13 @@ from __future__ import print_function
 
 import math
 
-from apiclient import errors
-from googleapiclient.discovery import build
+import pickle
+import os.path
 from googleapiclient.http import BatchHttpRequest
-from httplib2 import Http
-from oauth2client import client, file, tools
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from googleapiclient import errors
 
 USER_ID = 'me'
 # If modifying these scopes, delete the file token.json.
@@ -20,12 +22,26 @@ to_delete_ids = []
 
 
 def getService():
-    store = file.Storage('token.json')
-    creds = store.get()
-    if not creds or creds.invalid:
-        flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
-        creds = tools.run_flow(flow, store)
-    service = build('gmail', 'v1', http=creds.authorize(Http()))
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists("token.pickle"):
+        with open("token.pickle", "rb") as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('gmail', 'v1', credentials=creds)
     return service
 
 
