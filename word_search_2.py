dcb7bdf032a970710ee1f8a1a409d8d6eefa4c5a
Aritra Ghosh <aritraghosh4@kpmg.com>
2020-05-12 19:30:29 +0530

    - added leetcode word search problem

--- word_search_2.py
+++ word_search_2.py
@@ -1,45 +1,124 @@
+"""
+    Given a 2D board and a list of words from the dictionary, find
+    all words in the board. Each word must be constructed from letters of
+    sequentially adjacent cell, where "adjacent" cells are those horizontally
+    or vertically neighboring.The same letter cell may not be used more
+    than once in a word.
+
+    Example:
+    Input:
+        board = [
+                ['o', 'a', 'a', 'n'],
+                ['e', 't', 'a', 'e'],
+                ['i', 'h', 'k', 'r'],
+                ['i', 'f', 'l', 'v']
+            ]
+    words = ["oath", "pea", "eat", "rain"]
+
+    Output: ["eat", "oath"]
+
+    Note:
+        All inputs are consist of lowercase letters a - z.
+        The values of words are distinct.
+"""
 from typing import List
 
 
+class Node:
+    __slots__ = ["nodes", "is_leaf"]
+
+    def __init__(self):
+        self.nodes = {}
+        self.is_leaf = False
+
+
+class Trie:
+    def __init__(self):
+        self.root = Node()
+
+    def insert(self, word):
+        current = self.root
+
+        for idx, letter in enumerate(word):
+            if letter not in current.nodes:
+                current.nodes[letter] = Node()
+                if idx == len(word) - 1:
+                    current.nodes[letter].is_leaf = True
+            current = current.nodes[letter]
+
+
 class Solution:
-    def exist(self, board: List[List[str]], word: str) -> bool:
-        nrows = len(board)
-        ncols = len(board[0])
+    directions = {(0, 1), (-1, 0), (1, 0), (0, -1)}
 
-        def backtrack(i, j, idx):
-            char = board[i][j]
-            if char != word[idx]:
-                return False
-            elif idx == len(word) - 1:
-                return True
+    def is_cell_valid(self, row: int, col: int, max_row: int, max_col: int) -> bool:
+        return 0 <= row < max_row and 0 <= col < max_col
 
-            board[i][j] = ''
+    def find_helper(self, board: List[List[str]], word: str, row: int, col: int, index: int, trie: Node) -> str:
+        current_char = board[row][col]
+        # if current_char != word[index]:
+        #     return False
+        # elif len(word) - 1 == index:
+        #     return True
 
-            if i > 0 and backtrack(i - 1, j, idx + 1):
-                return True
-            if j > 0 and backtrack(i, j - 1, idx + 1):
-                return True
-            if i < nrows - 1 and backtrack(i + 1, j, idx + 1):
-                return True
-            if j < ncols - 1 and backtrack(i, j + 1, idx + 1):
-                return True
-            board[i][j] = char
+        if current_char not in trie.nodes:
             return False
 
-        for i in range(nrows):
-            for j in range(ncols):
-                if backtrack(i, j, 0):
-                    return True
+        if trie.nodes[current_char].is_leaf:
+            return True
 
-        return False
+        # replace current position
+        board[row][col] = "!"
+
+        ans = False
+        # iterate over all possible directions
+        for (x, y) in self.directions:
+            newr, newc = row + x, col + y
+            if self.is_cell_valid(newr, newc, len(board), len(board[0])) and \
+                    board[newr][newc] != "!":
+                ans |= self.find_helper(board, word, newr, newc, index + 1, trie.nodes[current_char])
+
+        # replace dummy var with actual, backtrack
+        board[row][col] = current_char
+        return ans
+
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        flag = False
+        trie = Trie()
+        for word in words:
+            trie.insert(word)
+        result = []
+        for word in words:
+            for row in range(len(board)):
+                for col in range(len(board[0])):
+                    if word[0] == board[row][col]:
+                        if self.find_helper(board, word, row, col, 0, trie.root):
+                            flag = True
+                            break
+                if flag:
+                    result.append(word)
+                    flag = False
+                    break
+        return result
 
 
 obj = Solution()
-grid = [
-    ['A', 'B', 'C', 'E'],
-    ['S', 'F', 'C', 'S'],
-    ['A', 'D', 'E', 'E']
+matrix = [
+    ['o', 'a', 'a', 'n'],
+    ['e', 't', 'a', 'e'],
+    ['i', 'h', 'k', 'r'],
+    ['i', 'f', 'l', 'v']
 ]
-obj = Solution()
-search_word = "ABCB"
-print(obj.exist(board=grid, word=search_word))  # False
+word_list = ["oath", "pea", "eat", "rain"]
+
+output = obj.findWords(matrix, word_list)  # Output: ["eat","oath"]
+assert output.sort() == ["eat", "oath"].sort(), "Wrong values returned"
+
+matrix = [
+    ["a", "b"],
+    ["c", "d"]
+]
+
+word_list = ["ab", "cb", "ad", "bd", "ac", "ca", "da", "bc", "db", "adcb", "dabc", "abb",
+             "acb"]  # Output: ["ab","ac","bd","ca","db"]
+output = obj.findWords(matrix, word_list)
+print(output)
