69eb3e3af0a151e1536588acf53459e4471020cb
Zooperman <joy.calcutta@gmail.com>
2020-06-01 22:30:44 +0530

    updated word_break_ii.py

--- word_break_ii.py
+++ word_break_ii.py
@@ -3,12 +3,10 @@ from typing import List
 
 class Solution:
     cache = dict()
-
-    # visited = set()
-
+    visited = set()
     def helper(self, input: str, end: int, word_dict: set):
         result = set()
-        visited = set()
+        # visited = set()
         if end <= 0:
             return ("",)
 
@@ -18,8 +16,8 @@ class Solution:
         start = 0
         while start < end:
             new_str = input[start: end]
-            if new_str in word_dict and new_str not in visited:
-                visited.add(new_str)
+            if new_str in word_dict and new_str not in self.visited:
+                self.visited.add(new_str)
                 tmp_list = self.helper(input, start, word_dict)
                 for char in tmp_list:
                     if len(char) == 0:
@@ -27,36 +25,31 @@ class Solution:
                     else:
                         result.add(char + " " + new_str)
             start += 1
-            # if new_str in visited:
-            #     visited.remove(new_str)
         self.cache[end] = result
-        # print(visited)
+        # self.visited.remove(result)
         return list(result)
 
     def wordbreak(self, string: str, wordDict: List[str]) -> List[str]:
-        self.cache.clear()
-        # self.visited.clear()
+        # self.cache.clear()
         wordDict = set(wordDict)
-        return self.helper(string, len(string), wordDict)
-
-    # def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
-    # dp solution
-    #     dp = defaultdict(list)
-    #     dp[0] = [""]
-    #     words = set(wordDict)
-    #
-    #     for i in range(1, len(s) + 1):
-    #         for j in range(i):
-    #             if not dp[j]:
-    #                 continue
-    #             sub_str = s[j:i]
-    #             if sub_str in words:
-    #                 dp[i].extend([c + " " + sub_str if c
-    #                               else sub_str for c in
-    #                               dp[j]])
-    #
-    #     return dp[len(s)]
-
+        # return self.helper(string, len(string), wordDict)
+      # dp solution
+        from collections import defaultdict
+        dp = defaultdict(list)
+        dp[0] = [""]
+        words = set(wordDict)
+    
+        for i in range(1, len(string) + 1):
+            for j in range(i):
+                if not dp[j]:
+                    continue
+                sub_str = s[j:i]
+                if sub_str in words:
+                    dp[i].extend([c + " " + sub_str if c
+                                  else sub_str for c in
+                                  dp[j]])
+    
+        return dp[len(s)]
 
 if __name__ == "__main__":
     obj = Solution()
@@ -64,14 +57,19 @@ if __name__ == "__main__":
     # word_dict = ["cat", "cats", "and", "sand", "dog"]
     # expected_output = True
 
-    # print(obj.wordBreak(s, word_dict))
+    # print(obj.wordbreak(s, word_dict))
 
-    # s = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-    # word_dict = ["a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa", "aaaaaaa", "aaaaaaaa", "aaaaaaaaa", "aaaaaaaaaa"]
-    #
-    # print(obj.wordBreak(s, word_dict))
-
-    s = "pineapplepenapple"
-    word_dict = ["apple", "pen", "applepen", "pine", "pineapple"]
-
+    s = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    word_dict = ["a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa", "aaaaaaa", "aaaaaaaa", "aaaaaaaaa", "aaaaaaaaaa"]
+    
     print(obj.wordbreak(s, word_dict))
+
+    # s = "pineapplepenapple"
+    # word_dict = ["apple", "pen", "applepen", "pine", "pineapple"]
+
+    # print(obj.wordbreak(s, word_dict))
+
+    # s = "catsandog"
+    # word_dict = ["cat", "cats", "and", "sand", "dog"]
+
+    # print(obj.wordbreak(s, word_dict))
