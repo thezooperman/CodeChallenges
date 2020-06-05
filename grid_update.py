be791a76416e29c1720fba0de7e81e2e1a0d09cb
Zooperman <joy.calcutta@gmail.com>
2020-06-05 08:12:27 +0530

    re-factored code to use loops instead of recursion via bfs

--- grid_update.py
+++ grid_update.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 def update_grid(rows, columns, matrix):
     """
         Given a grid of cells, which has value 0 or 1
@@ -55,10 +58,63 @@ def update_grid(rows, columns, matrix):
                 updated_server = False
     return min_days
 
+visited = set()
+directions = [(0, -1), (0, 1), (-1, 0), (1, 0)]
+
+
+def grid_update_helper(row, col, grid):
+    min_count = 0
+    queue = deque([(row, col)])
+
+    old_visited = len(visited)
+
+    while queue:
+        row, col = queue.popleft()
+
+        for (x, y) in directions:
+            new_row, new_col = row + x, col + y
+            if 0 <= new_row < len(grid) and \
+                0 <= new_col < len(grid[0]) and \
+                    grid[new_row][new_col] == 0 and \
+            (new_row, new_col) not in visited:
+                visited.add((new_row, new_col))
+                queue.append((new_row, new_col))
+
+    return 1
+
+
+def grid_update(grid):
+    min_days = 0
+    visited.clear()
+    for row in range(len(grid)):
+        for col in range(len(grid[0])):
+            if (row, col) not in visited and grid[row][col] == 0:
+                visited.add((row, col))
+                min_days += grid_update_helper(row, col, grid)
+
+    return min_days
+
 
 if __name__ == '__main__':
-    grid = [[0, 1, 0, 1, 0, 1],
-            [0, 0, 0, 1, 0, 1],
-            [1, 1, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 1]]
-    print(f'Total hours:', update_grid(4, 6, grid))
+    # grid = [[0, 1, 0, 1, 0, 1],
+    #         [0, 0, 0, 1, 0, 1],
+    #         [1, 1, 0, 0, 0, 0],
+    #         [0, 0, 0, 0, 0, 1]]
+    # print(f'Total hours:', update_grid(4, 6, grid))
+
+    # grid = [
+    #     [0, 1, 1, 0, 1],
+    #     [0, 1, 0, 1, 0],
+    #     [0, 0, 0, 0, 1],
+    #     [0, 1, 0, 0, 0]
+    # ]
+
+    # print(grid_update(grid)) # output = 3
+
+    grid = [
+        [1, 1, 1, 1],
+        [1, 1, 1, 1],
+        [1, 1, 1, 1],
+        [1, 1, 0, 1]
+    ]
+    print(grid_update(grid))  # output = 1
