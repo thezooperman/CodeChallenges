3b23ce0361355f000da7d42e6279c5f0f424e722
Zooperman <joy.calcutta@gmail.com>
2020-08-25 08:45:13 +0530

    Changes: - re-factored boggle.py

--- boggle.py
+++ boggle.py
@@ -1,5 +1,6 @@
 from typing import List
 
+
 class TrieNode:
     def __init__(self):
         self.key = None  # key - a,b,c
@@ -47,7 +48,8 @@ def is_safe(matrix, row, col, visited):
 
 def searchWord(matrix: List[List[int]], trie: TrieNode, tmp_str: str, row: int, col: int, visited: set, dictionary: set):
     if trie.is_end and tmp_str in dictionary:
-        print(tmp_str, sep=' ', end=' ')
+        # print(tmp_str, sep=' ', end=' ')
+        result.add(tmp_str)
         dictionary.remove(tmp_str.strip())
         return
 
@@ -64,16 +66,23 @@ def searchWord(matrix: List[List[int]], trie: TrieNode, tmp_str: str, row: int, 
         visited.remove((row, col))
 
 
+result = set()
+
+
 def driver():
     testCases = 1  # int(input().strip())
     for tc in range(testCases):
-        x = 4  # int(input().strip())
+        x = 6  # 4  # int(input().strip())
         # set(input().strip().split(None))
-        dictionary = {"GEEKS", "FOR", "QUIZ", "GO"}
+        # dictionary = {"GEEKS", "FOR", "QUIZ", "GO"}
+        dictionary = {'dfd', 'ded', 'fd', 'e', 'dec', 'df'}
         # dimension = input().strip().split(None)
-        n, m = 3, 3  # int(dimension[0]),int(dimension[1])
-        board_values = ['G', 'I', 'Z', 'U', 'E', 'K',
-                        'Q', 'S', 'E']  # input().strip().split(None)
+        # n, m = 3, 3  # int(dimension[0]),int(dimension[1])
+        n, m = 4, 2
+        # board_values = ['G', 'I', 'Z', 'U', 'E', 'K',
+        #                 'Q', 'S', 'E']  # input().strip().split(None)
+
+        board_values = ['f', 'f', 'd', 'e', 'f', 'b', 'b', 'e']
 
         visited = set()
         trie = Trie()
@@ -91,6 +100,7 @@ def driver():
             start, end = end, end + m
             row += 1
 
+        # print(*matrix)
         # go through matrix for word
         visited = set()
         trie = Trie()
@@ -98,6 +108,9 @@ def driver():
         # build trie
         for word in dictionary:
             trie.insert(word)
+
+        # go through matrix for word
+        visited = set()
 
         tmp_str = ''
         for row in range(n):
@@ -108,6 +121,8 @@ def driver():
                         matrix, trie.root.edges[matrix[row][col]], tmp_str, row, col, visited, dictionary)
                     tmp_str = ''
 
+        print(*sorted(result))
+        result.clear()
         print(flush=True)
 
 
